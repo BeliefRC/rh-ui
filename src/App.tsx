0b0dc238e7ed0d9d5aac14d1d160302d08b23a86
BeliefRC <beliefrc@outlook.com>
2020-06-02 23:46:03 +0800

    chore：初始化项目基本内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello world</h1>
+        <h2>hello world</h2>
+        <h3>hello world</h3>
+        <h4>hello world</h4>
+        <h5>hello world</h5>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
